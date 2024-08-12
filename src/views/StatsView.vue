23fc0fe4742595183f8b6f567a42f6e5c0e85cc0
abdelraoufstoryteller <abdelraouf@getstoryteller.com>
2024-08-12 23:31:39 +0300

    :package: chore: Update project configuration and file structure

---
+++ src/views/StatsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="notes">
+    <h1>Stats</h1>
+  </div>
+</template>
+
+<script setup>
+// <script setup> is a new feature in Vue 3.2 that allows you to write the SFC's <script> section in a more concise way.
+</script>
+
+<style lang="scss" scoped>
+// Scoped CSS is a feature of Vue that allows you to write CSS that only applies to the current component.
+</style>
